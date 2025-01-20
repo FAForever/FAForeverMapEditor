7863fe14f2ddc67c8fa9aa9b71b451951d2e5ae6
BlackYps <jonas-fb@gmx.de>
2025-01-20 13:55:21 +0100

    Clean up layer settings editor layout

--- Assets/Scripts/UI/Tools/Terrain/TerrainTypes/Editor/LayerSettingsEditor.cs
+++ Assets/Scripts/UI/Tools/Terrain/TerrainTypes/Editor/LayerSettingsEditor.cs
@@ -16,16 +16,10 @@ namespace EditMap.TerrainTypes.Editor
         private float colorWidth = 100f;
         private float blockingWidth = 60f;
         private float styleWidth = 110f;
-        //private float descriptionWidth = 0.2f;
-        private float descriptionHeight = 1 - 0.25f;
-        //private float descriptionButtonWidth = 0.2f;
+        private float fieldHeight = 18f;
 
-        //private static Vector4 border = new Vector4(0, 3, 0, 3);
+        private static float defaultElementHeight = 45f;
 
-        private static float defaultElementHeight = 21f;
-        private static float selectedElementHeight = defaultElementHeight * 4;
-
-        //private int selectedIndex = -1;
 
         private void OnEnable()
         {
@@ -57,12 +51,6 @@ namespace EditMap.TerrainTypes.Editor
 
         public override void OnInspectorGUI()
         {
-//            EditorGUILayout.FloatField("elementHeight", reorderableList.elementHeight);
-//            nameWidth = EditorGUILayout.FloatField("nameWidth", nameWidth);
-//            indexWidth = EditorGUILayout.FloatField("indexWidth", indexWidth);
-//            colorWidth = EditorGUILayout.FloatField("colorWidth", colorWidth);
-//            blockingWidth = EditorGUILayout.FloatField("blockingWidth", blockingWidth);
-//            styleWidth = EditorGUILayout.FloatField("styleWidth", styleWidth);
             reorderableList.DoLayoutList();
         }
 
@@ -75,64 +63,32 @@ namespace EditMap.TerrainTypes.Editor
             var blockingProperty = property.FindPropertyRelative("blocking"); //blocking
             var styleProperty = property.FindPropertyRelative("style"); //style
             var descriptionProperty = property.FindPropertyRelative("description"); //description
-//            name
-//            index
-//            color
-//            blocking
-//            style
-//            description
+
             rect.yMin += 3;
             rect.yMax -= 3;
             using (var scope = new EditorGUI.ChangeCheckScope())
             {
-                if (!active)
-                {
-                    float shift = rect.x;
-                    EditorGUI.PropertyField(new Rect(shift, rect.y, nameWidth, rect.height), nameProperty,
-                        GUIContent.none);
-                    shift += nameWidth;
-                    EditorGUI.PropertyField(new Rect(shift, rect.y, indexWidth, rect.height), indexProperty,
-                        GUIContent.none);
-                    shift += indexWidth;
-                    EditorGUI.PropertyField(new Rect(shift, rect.y, blockingWidth, rect.height), blockingProperty,
-                        GUIContent.none);
-                    shift += blockingWidth;
-                    EditorGUI.PropertyField(new Rect(shift, rect.y, styleWidth, rect.height), styleProperty,
-                        GUIContent.none);
-                    shift += styleWidth;
-                    EditorGUI.PropertyField(new Rect(rect.xMax - colorWidth, rect.y, colorWidth, rect.height),
-                        colorProperty, GUIContent.none);
-                    shift += colorWidth;
-                }
-                else
-                {
+               
                     float shift = rect.x;
                     EditorGUI.PropertyField(
-                        new Rect(shift, rect.y, nameWidth, rect.height * (1 - descriptionHeight) - 3), nameProperty,
-                        GUIContent.none);
-                    shift += nameWidth;
+                        new Rect(shift, rect.y, nameWidth, fieldHeight), nameProperty, GUIContent.none);
+                    shift += nameWidth + 2;
                     EditorGUI.PropertyField(
-                        new Rect(shift, rect.y, indexWidth, rect.height * (1 - descriptionHeight) - 3), indexProperty,
-                        GUIContent.none);
-                    shift += indexWidth;
+                        new Rect(shift, rect.y, indexWidth, fieldHeight), indexProperty, GUIContent.none);
+                    shift += indexWidth + 2;
                     EditorGUI.PropertyField(
-                        new Rect(shift, rect.y, blockingWidth, rect.height * (1 - descriptionHeight) - 3),
-                        blockingProperty, GUIContent.none);
+                        new Rect(shift, rect.y, blockingWidth, fieldHeight), blockingProperty, GUIContent.none);
                     shift += blockingWidth;
                     EditorGUI.PropertyField(
-                        new Rect(shift, rect.y, styleWidth, rect.height * (1 - descriptionHeight) - 3), styleProperty,
-                        GUIContent.none);
+                        new Rect(shift, rect.y, styleWidth, fieldHeight), styleProperty, GUIContent.none);
                     shift += styleWidth;
                     EditorGUI.PropertyField(
-                        new Rect(rect.xMax - colorWidth, rect.y, colorWidth, rect.height * (1 - descriptionHeight) - 3),
-                        colorProperty, GUIContent.none);
+                        new Rect(rect.xMax - colorWidth, rect.y, colorWidth, fieldHeight), colorProperty, GUIContent.none);
                     shift += colorWidth;
-
 
                     descriptionProperty.stringValue = EditorGUI.TextArea(
-                        new Rect(rect.x, rect.y + rect.height * (1 - descriptionHeight), rect.width,
-                            rect.height * descriptionHeight), descriptionProperty.stringValue);
-                }
+                        new Rect(rect.x, rect.y + fieldHeight + 3, rect.width, fieldHeight), descriptionProperty.stringValue);
+                
 
                 if (scope.changed)
                 {
@@ -143,11 +99,6 @@ namespace EditMap.TerrainTypes.Editor
 
         private float GetElementHeight(int index)
         {
-            if (reorderableList.index == index)
-            {
-                return selectedElementHeight;
-            }
-
             return defaultElementHeight;
         }
 
@@ -168,12 +119,6 @@ namespace EditMap.TerrainTypes.Editor
 
         private void OnAddElement(ReorderableList list)
         {
-//            foreach (TerrainTypeLayerSettings layerSettings in list.list)
-//            {
-//                layerSettings.index
-//            }
-
-//            list.list.Add(new TerrainTypeLayerSettings());
         }
 
         private void DrawElementDescription(Rect rect, int index, bool active, bool focused)
