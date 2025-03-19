f11dc92db2d74645a34c1981514525c5613240ee
BlackYps <jonas-fb@gmx.de>
2025-03-19 23:51:48 +0100

    Fix the fucking newline bug for real this time

--- Assets/Scripts/Ozone SCMAP Code/Lua/ScenarioLua.cs
+++ Assets/Scripts/Ozone SCMAP Code/Lua/ScenarioLua.cs
@@ -472,7 +472,10 @@ namespace MapLua
 			LuaTable ScenarioInfoTab = LuaFile.GetTable(TABLE_SCENARIOINFO);
 
 			Data.name = LuaParser.Read.StringFromTable(ScenarioInfoTab, ScenarioInfo.KEY_NAME);
-			Data.description = LuaParser.Read.StringFromTable(ScenarioInfoTab, ScenarioInfo.KEY_DESCRIPTION);
+			// When there is \r\n in the scenario source file (for example from a neroxis map generator map)
+			// we need to get rid of that, or we will break the formatting of the file when we save it again.
+			// The multiline text field of unity doesn't handle it well.
+			Data.description = LuaParser.Read.StringFromTable(ScenarioInfoTab, ScenarioInfo.KEY_DESCRIPTION).Replace("\r", "");
 			Data.type = LuaParser.Read.StringFromTable(ScenarioInfoTab, ScenarioInfo.KEY_TYPE);
 			Data.starts = LuaParser.Read.BoolFromTable(ScenarioInfoTab, ScenarioInfo.KEY_STARTS, true);
 
